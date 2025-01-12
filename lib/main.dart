af3448e8d61039e81412fa4ec4ee27e7fa367efc
6519c10007 <s6519c10007@sau.ac.th>
2025-01-12 15:17:41 +0700

    v.1 create project and set value for new project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iot_sau_pm_project/views/home_ui.dart';
+import 'package:iot_sau_pm_project/views/splash_screen_ui.dart';
+
+void main() {
+  runApp(
+    //13131
+    IoTSauPMproject(),
+  );
+}
+
+//----------------------------------------------------------------
+class IoTSauPMproject extends StatefulWidget {
+  const IoTSauPMproject({super.key});
+
+  @override
+  State<IoTSauPMproject> createState() => _IoTSauPMprojectState();
+}
+
+class _IoTSauPMprojectState extends State<IoTSauPMproject> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenUI(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+    );
+  }
+}
